9fdceca87d0c602938c7f5903b16b5e9b9764489
SamirCaro <Samir.caro@ado-tech.com>
2026-03-02 17:32:22 -0500

    Configuracion contexto

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using ApiEstudio.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiEstudio.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options) 
+        { 
+        }
+        
+        //Aqui pasar todas las entidades (modelos)
+
+        public DbSet<Categoria> Categoria { get; set; }
+            
+     
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,13 +1,41 @@
+using System.Data.SqlClient;
+using ApiEstudio.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexionSql")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+
 var app = builder.Build();
+
+//var logger = app.Logger;
+
+//// Obtener la cadena de conexión
+//string connectionString = builder.Configuration.GetConnectionString("ConexionSQL");
+
+//// Probar la conexión usando logger
+//try
+//{
+//    using var connection = new SqlConnection(connectionString);
+//    connection.Open();
+//    logger.LogInformation("Conexión a base de datos exitosa");
+//}
+//catch (Exception ex)
+//{
+//    logger.LogError(ex, "Error al conectar a la base de datos");
+//}
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
