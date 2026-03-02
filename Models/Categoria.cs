8e1b13fda33479ef68233161524d7211c0ac9ee8
SamirCaro <Samir.caro@ado-tech.com>
2026-03-02 11:56:16 -0500

    modelo categoria

---
+++ Models/Categoria.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ApiEstudio.Models
+{
+    public class Categoria
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        [Display(Name="Fecha Creacion")]
+        public DateTime FechaCreacion { get; set; }
+    }
+}
